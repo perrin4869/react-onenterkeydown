1d646fd07ff1f4a54ff67b3fb21ccf67a73eb10d
Julian Grinblat <julian@dotcore.co.il>
2016-05-12 02:04:50 +0900

    Add gulp test task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
+const util = require('gulp-util');
 const babel = require('gulp-babel');
+const mocha = require('gulp-mocha');
 const eslint = require('gulp-eslint');
+const compiler = require('babel-core/register');
 
 const src = 'src/*.js';
 
@@ -10,7 +13,18 @@ gulp.task('lint', () => (
   .pipe(eslint.format())
 ));
 
-gulp.task('build', ['lint'], () => (
+gulp.task('test', ['lint'], () => (
+  gulp.src('test')
+  .pipe(mocha({
+    reporter: 'spec',
+    compilers: {
+      js: compiler,
+    },
+  }))
+  .on('error', util.log)
+));
+
+gulp.task('build', ['test'], () => (
   gulp.src(src)
   .pipe(babel())
   .pipe(gulp.dest('lib'))
